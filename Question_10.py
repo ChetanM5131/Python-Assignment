c540c98c5a31c4eb8dca8084eaada16bc2a83f08
ChetanM5131 <chetan.mundhe@gmail.com>
2026-06-13 12:06:35 +0530

    Delete Question_10.py

--- Question_10.py
+++
@@ -1,8 +0,0 @@
-# What will be output
-print("10"+"20")
-print(10 + 20)
-#Output
-#1020
-#30
-#in first print statement we have values as string and '+' wored as concat operator and performed string concatination.
-#in second print statement we have values as int and '+' worke as arithmatic operator and perfored addition
