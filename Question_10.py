a1d1e299b51a15c4faf579900069e8a6ef5887e0
ChetanM5131 <chetan.mundhe@gmail.com>
2026-06-13 11:58:54 +0530

    Add files via upload

---
+++ Question_10.py
@@ -0,0 +1,8 @@
+# What will be output
+print("10"+"20")
+print(10 + 20)
+#Output
+#1020
+#30
+#in first print statement we have values as string and '+' wored as concat operator and performed string concatination.
+#in second print statement we have values as int and '+' worke as arithmatic operator and perfored addition
---
+++ Question_7.py
@@ -0,0 +1,10 @@
+# Why does input() always return string? How can you convert it?
+val_1=input("Enter Data: ")
+print(type(val_1))
+#Output:
+#Enter Data: 1@str
+#<class 'str'>
+
+# input() gets or read input from user which is nothig but string of characters thus input() always retun string 
+# as string datatype can hold number,character,special symbols.
+# We can using built in type cast functions to convert it into useful datatype
