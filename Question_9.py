d0f8c936ae11f877c18c28381a0db2d7a89f159f
ChetanM5131 <chetan.mundhe@gmail.com>
2026-06-13 12:07:08 +0530

    Delete Question_9.py

--- Question_9.py
+++
@@ -1,6 +0,0 @@
-# Write a program to take user's name and age and display
-# Hello <Name>, you will turn <age+1> next year
-User_name=input("Name:-")
-User_age=int(input("Age:-"))
-User_age= User_age+1
-print(f"Hello {User_name} ,you will turn {User_age} next year")
