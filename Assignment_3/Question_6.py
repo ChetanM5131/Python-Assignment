c6d701af398527889d203f9942294d03432522cc
ChetanM5131 <chetan.mundhe@gmail.com>
2026-06-22 09:27:49 +0530

    Add files via upload

--- Assignment_3/Question_6.py
+++ Assignment_3/Question_6.py
@@ -4,3 +4,8 @@ var1=input()
 print("Data Type is",type(var1))
 print("Address is",id(var1))
 print("Size is ",getsizeof(var1))
+#Outptu
+#Enter Variable value:15
+#Data Type is <class 'str'>
+#Address is 2284362419968
+#Size is  43
